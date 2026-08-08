fbdb3ac95f243e687eacccd68733e85dd54b7a79
Alex Bessa <alexbessa.dev@gmail.com>
2026-08-08 11:47:07 -0300

    feat: implementar interface de programacao de aplicacao minimalista

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 import http from 'node:http';
 
-http.createServer(() => {
-  console.log('Knocked on the door.');
+const users = [{
+  name: 'Alex Bessa',
+  email: 'alex@email.com'
+}, {
+  name: 'Valdiana Bessa',
+  email: 'valdiana@email.com'
+}, {
+  name: 'Ana Bessa',
+  email: 'ana@email.com'
+}];
+
+http.createServer((request, response) => {
+  response.writeHead(
+    200,
+    { "content-type": "application/json" }
+  );
+  response.end(JSON.stringify(users));
 }).listen(3000);
