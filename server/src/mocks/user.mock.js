84848b3d6446a51c50990f932bc53f3bb3c6a8bb
Alex Bessa <alexbessa.dev@gmail.com>
2026-08-29 11:30:59 -0300

    refactor: implementar estrutura organizacional

---
+++ server/src/mocks/user.mock.js
@@ -0,0 +1,12 @@
+const users = [{
+  name: 'Alex Bessa',
+  email: 'alex@email.com'
+}, {
+  name: 'Valdiana Bessa',
+  email: 'valdiana@email.com'
+}, {
+  name: 'Ana Bessa',
+  email: 'ana@email.com'
+}];
+
+export default users;
